3398fb5d822507e2256852e8e86a4e5f6c713211
vatsal-sodha <vatsalsodha150297@gmail.com>
2025-01-11 02:00:27 -0600

    add account modal

--- bankbuddyui/src/AccountsTab.jsx
+++ bankbuddyui/src/AccountsTab.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
-import { Button, Paper } from '@mui/material';
+import {React, useState} from 'react';
+import { Button, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import AddAccountDialog from './AddAcountDialog';
 
 const AccountsTab = () => {
-  return (
-    // <Paper sx={{ p: 2 }}>
-      <Button
-        variant="contained"
-        startIcon={<AddIcon />}
-        sx={{ mb: 2 }}
-      >
-        Add Account
-      </Button>
-    //   {/* Add your accounts list/grid here */}
-    // </Paper>
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpenDialog(true);
+    };
+  
+    const handleClose = () => {
+      setOpenDialog(false);
+    };
+
+    const handleSubmit = (formData) => {
+        console.log('Form submitted:', formData);
+        // Here you would typically send the data to your backend
+        setOpenDialog(false);
+      };
+    return (
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end', mb: 2 }}>
+        <Button
+            variant="contained"
+            startIcon={<AddIcon />}
+            onClick={handleClickOpen}
+        >
+            Add Account
+        </Button>
+        <AddAccountDialog
+            open={openDialog}
+            onClose={handleClose}
+            onSubmit={handleSubmit}
+        />
+        {/* Add your accounts list/grid here */}
+        </Box>
   );
 };
 
---
+++ bankbuddyui/src/AddAcountDialog.jsx
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+
+const AddAccountDialog = ({ open, onClose, onSubmit }) => {
+  const [formData, setFormData] = useState({
+    account_name: '',
+    institution: '',
+    account_type: '',
+    last_4_digit: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(formData);
+    setFormData({
+      account_name: '',
+      institution: '',
+      account_type: '',
+      last_4_digit: ''
+    });
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+      <form onSubmit={handleSubmit}>
+        <DialogTitle>Add New Account</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="account_name"
+            label="Account Name"
+            type="text"
+            fullWidth
+            value={formData.account_name}
+            onChange={handleChange}
+            required
+            sx={{ mb: 2 }}
+          />
+          <TextField
+            margin="dense"
+            name="institution"
+            label="Institution"
+            type="text"
+            fullWidth
+            defaultValue="Chase, Bank of America, etc"
+            value={formData.institution}
+            onChange={handleChange}
+            required
+            sx={{ mb: 2 }}
+          />
+          <FormControl fullWidth required sx={{ mb: 2 }}>
+            <InputLabel>Account Type</InputLabel>
+            <Select
+              name="account_type"
+              value={formData.account_type}
+              label="Account Type"
+              onChange={handleChange}
+            >
+              <MenuItem value="credit/debit">Credit/Debit Card</MenuItem>
+              <MenuItem value="checking/savings">Checking/Savings Account</MenuItem>
+              <MenuItem value="other">Other</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            margin="dense"
+            name="last_4_digit"
+            label="Last 4 Digits"
+            type="text"
+            fullWidth
+            value={formData.last_4_digit}
+            onChange={handleChange}
+            required
+            inputProps={{
+              maxLength: 4,
+              pattern: "[0-9]*"
+            }}
+            helperText="Enter the last 4 digits of your account"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <Button type="submit" variant="contained">Submit</Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+};
+
+export default AddAccountDialog;
